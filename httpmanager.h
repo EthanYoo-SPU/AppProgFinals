1732c85a4374dd0cb5753d666cd18e3af63d4c3f
EthanYoo-SPU <66099643+EthanYoo-SPU@users.noreply.github.com>
2020-06-15 14:54:16 -0700

    Add files via upload
    Added weather functionality that tells accurate weather type and time based on a zip code. Images to be added later.

--- httpmanager.h
+++ httpmanager.h
@@ -13,17 +13,21 @@ public:
     explicit HTTPManager(QObject *parent = nullptr);
     ~HTTPManager();
     void sendImageRequest(QString zip);
+    void sendWeatherRequest(QString zip);
 
 signals:
     void ImageReady(QPixmap *image);
-
+    void WeatherJsonReady(QJsonObject *json);
 
 private slots:
     void ImageDownloadedHandler(QNetworkReply *reply);
+    void WeatherDownloadHandler(QNetworkReply *reply);
+
 
 private:
     QNetworkAccessManager *imageDownloadManager;
     QByteArray downloadedData;
+    QNetworkAccessManager *weatherAPIManager;
 };
 
 #endif // HTTPMANAGER_H
